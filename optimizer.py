e1f4149f852b88b5ed9bebf156bed64146db6776
lukada <l.adamowicz4@gmail.com>
2026-01-04 17:12:27 +0100

    MSAM

--- optimizer.py
+++ optimizer.py
@@ -1,4 +1,51 @@
-from torch.optim import Optimizer
+from typing import Optional, Callable
+
+import torch
 
 
-class SAM(Optimizer):
+class AdamW_MSAM(torch.optim.Optimizer):
+    def __init__(
+            self,
+            params,
+            lr=1e-3,
+            betas=(0.9, 0.999),
+            eps=1e-8,
+            weight_decay=1e-2,
+            maximize: bool = False,
+            rho = 0.05,
+            ):
+
+        if lr < 0.0:
+            raise ValueError(f"Invalid learning rate: {lr}")
+        if not 0.0 <= betas[0] < 1.0:
+            raise ValueError(f"Invalid beta parameter at index 0: {betas[0]}")
+        if not 0.0 <= betas[1] < 1.0:
+            raise ValueError(f"Invalid beta parameter at index 1: {betas[1]}")
+        if eps < 0.0:
+            raise ValueError(f"Invalid epsilon value: {eps}")
+        if weight_decay < 0.0:
+            raise ValueError(f"Invalid weight_decay value: {weight_decay}")
+        if rho < 0.0:
+            raise ValueError('rho should be non-negative')
+        defaults = dict(
+            lr=lr,
+            betas=betas,
+            eps=eps,
+            weight_decay=weight_decay,
+            maximize=maximize,
+            rho=rho,
+        )
+        super(AdamW_MSAM, self).__init__(params, defaults)
+
+    def step(self, closure: Optional[Callable[[], float]] = None) -> Optional[float]:
+        for group in self.param_groups:
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                state = self.state[p]
+                if len(state) == 0:
+                    state['step'] = 0
+                    state['exp_avg'] = torch.zeros_like(p, memory_format=torch.preserve_format)
+                    state['exp_avg_sq'] = torch.zeros_like(p, memory_format=torch.preserve_format)
+
+                state['step'] += 1
