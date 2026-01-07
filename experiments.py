70786b309b707a67eaefe94faf3457c57ad76b40
L Adamowicz <l.adamowicz4@gmail.com>
2026-01-07 20:37:24 +0100

    conf

---
+++ experiments.py
@@ -0,0 +1,14 @@
+
+@dataclass
+class TrainConfig:
+    optimizer_name: str = "adamw"
+    lr: float = 0.0005
+    weight_decay: float = 2.0
+    rho: float = 0.05
+    num_epochs: int = 14000
+    batch_size: int = 10000
+    modulus: int = 97
+    seed: int = 42
+    run_name: str = ""
+    # model
+    # dataset/dataloader
